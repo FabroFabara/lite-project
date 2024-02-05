4c8f398935624b8fd4e3677a1541e6c6a5dc82b9
Fabricio Fabara <fabriciofabara@gmail.com>
2024-02-05 18:56:30 -0500

    Adding the beginning of the gradle project

---
+++ src/main/java/com/lite/backend/liteproject/LiteProjectApplication.java
@@ -0,0 +1,14 @@
+package com.lite.backend.liteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteProjectApplication.class, args);
+        System.out.println("Hello Fabricio the Lite Project is running...");
+    }
+
+}
---
+++ src/test/java/com/lite/backend/liteproject/LiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lite.backend.liteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
